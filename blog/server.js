b6ec99d713806cd4902fbc8a53f2e9cc35a9839c
Saif5017 <Saif50172169@users.noreply.github.com>
2020-12-14 22:10:08 +0530

    heroku fix

--- blog/server.js
+++ blog/server.js
@@ -1,35 +1,37 @@
 const path = require('path')
-const multer=require('multer')
-const csrf=require('csurf')
-const flash=require('connect-flash')
+const serverless=require('serverless-http');
+const compression = require('compression')
+const multer = require('multer')
+const csrf = require('csurf')
+const flash = require('connect-flash')
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
-const MongoDBStore=require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
 
-const MongoDB_URI='mongodb+srv://turd_waffle:SaifKhan@cluster0.lltqs.mongodb.net/blogworm?';
-const csrfProtection=csrf({})
+const MongoDB_URI = 'mongodb+srv://turd_waffle:SaifKhan@cluster0.lltqs.mongodb.net/blogworm?';
+const csrfProtection = csrf({})
 
 const app = express();
-const store=new MongoDBStore({
-    uri:MongoDB_URI,
-    collections:'sessions'
+const store = new MongoDBStore({
+    uri: MongoDB_URI,
+    collections: 'sessions'
 })
 
-const fileStorage=multer.diskStorage({
-    destination:(req, file, cb)=>{
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
         cb(null, 'images')
     },
-    filename:(req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
-})//this is used to store and fetch the name of the file
+}) //this is used to store and fetch the name of the file
 //mimetype is a feature that helps us to recognize the file type
-const fileFilter=(req, file, cb)=>{
-    if(file.mimetype==='image/png' || file.mimetype==='image/jpg' || file.mimetype==='image/jpeg'){
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
         cb(null, true)
-    }else{
+    } else {
         cb(null, false)
     }
 }
@@ -38,13 +40,22 @@ const errorController = require('./controllers/error')
 const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/images',express.static(path.join(__dirname, 'images')))
-app.use(multer({storage:fileStorage, fileFilter:fileFilter}).single('image'))
-app.use(session({secret:'my save', resave:false, saveUninitialized:false, store:store}));
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'))
+app.use(session({
+    secret: 'my save',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
 
 app.use(flash());
 app.use(csrfProtection)
@@ -56,33 +67,39 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use((req, res, next) => {
-    if(!req.session.user){
+    if (!req.session.user) {
         return next()
     }
     User.findById(req.session.user._id)
-    .then(result => {
-        req.user = result
-        next()
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(result => {
+            req.user = result
+            next()
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
-app.use((req, res, next)=>{
-    res.locals.isAuthenticated=req.session.isLoggedIn;
-    res.locals.csrfToken=req.csrfToken();
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
     next();
 })
 
-app.use(adminRoutes)
+app.use('./netlify/functions', adminRoutes)
 app.use(authRoutes);
 app.use(errorController.get404)
 
-mongoose.connect(MongoDB_URI)
+mongoose.connect(MongoDB_URI, {
+        useNewUrlParser: true
+    })
     .then(result => {
-        app.listen(3000)
+        app.listen(process.env.PORT || 3000, function () {
+            console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+        });
     })
     .catch(err => {
         console.log(err)
     })
+
+module.exports.handler=serverless(app)
